f60eca4875f9d6599053e75870f951246bd68ee3
Sourabh Mandal <sourabhmandal008@gmail.com>
2023-01-07 20:02:27 +0530

    primenumber

---
+++ temp.js
@@ -0,0 +1,15 @@
+function identifyPrime(num) {
+    
+    let factor=0;
+    
+    for(let i=1; i<=num; i++){
+        if(num%i==0){
+            factor++;
+        }
+    }
+    if(factor==2){
+        console.log("Yes")
+    }else{
+    console.log("No")
+}
+}
